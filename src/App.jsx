38e6152bf206b22bdd7af50dd26a570966de205d
Samet Aslan <sametaslan1710@gmail.com>
2023-12-22 01:17:39 +0300

    add: components and bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header'
+import Content from './Content'
+import Footer from './Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-     <main>
-      Hello World
-     </main>
-    </>
+    <div>
+      <Header />
+      <Content />
+      <Footer/>
+    </div>
   )
 }
 
